a313402df573129913aa2acec59191ac3b7bd486
Naufal Prima Yoriko <primayoriko@gmail.com>
2020-06-09 08:11:45 +0700

    init

---
+++ ASP-Agate-01/Data/ASP_Agate_01Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP_Agate_01.Models
+{
+    public class ASP_Agate_01Context : DbContext
+    {
+        public ASP_Agate_01Context (DbContextOptions<ASP_Agate_01Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ASP_Agate_01.Models.Student> Student { get; set; }
+    }
+}
---
+++ ASP-Agate-01/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_Agate_01.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public String Name { get; set; }
+        public int ClassNumber { get; set; }
+        public int Grade { get; set; }
+        public Student() { }
+    }
+}
